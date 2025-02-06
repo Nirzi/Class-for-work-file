93a52743d6f06916f42516cad2e1a1dcf481b228
Nirzi <nirzzii@mail.ru>
2025-02-06 20:42:53 +0500

    Добавлен класс JsonFile с методами

--- file_class.py
+++ file_class.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import json
+
 
 
 class AbstractFile(ABC):
@@ -22,3 +24,32 @@ class AbstractFile(ABC):
     def append(self, data):
         """Добавление данных в файл (без удаления существующего)"""
         pass
+    
+class JsonFile(AbstractFile):
+    """Класс для работы с JSON-файлами"""
+    def read(self):
+        """Чтение данных из JSON-файла"""
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return None
+        except json.JSONDecodeError:
+            return {}
+
+    def write(self, data):
+        """Запись данных в JSON-файл (перезапись)"""
+        with open(self.file_path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+    def append(self, data):
+        """Добавление данных в JSON-файл"""
+        existing_data = self.read()
+        if isinstance(existing_data, dict) and isinstance(data, dict):
+            existing_data.update(data)
+        elif isinstance(existing_data, list) and isinstance(data, list):
+            existing_data.extend(data)
+        else:
+            existing_data = data
+
+        self.write(existing_data)
